1917a99fa0f94c75ce462b33e8d370c60b7d5bff
BhagvatDeshmukh <134522509+BhagvatDeshmukh@users.noreply.github.com>
2025-11-21 16:09:25 +0530

    Update App.jsx

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -7,8 +7,32 @@ const socket = io.connect("https://chat-app-server-00jj.onrender.com");
 
 function App() {
   const room = "SSG";
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const joinRoom = () => {
+      socket.emit("join_room", room);
+      setShowChat(true);
+  };
+
+    useEffect(() => {
+
+    socket.on("msg_history", (history) => {
+      setMessageHistory((list) => [...list, history]);
+    });
+
+  }, [socket]);
+  
   return (
-        <Chat socket={socket} username={username} room={room} />
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <button onClick={joinRoom}>Join Chat</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} msgHistory={MessageHistory}/>
+      )}
+    </div>
   );
 }
 
