6f1a4d71f460cee1508a85056e93a38f6ca18836
BhagvatDeshmukh <134522509+BhagvatDeshmukh@users.noreply.github.com>
2025-11-21 15:38:30 +0530

    Simplify App component by removing state management
    Removed username and room state management, and simplified the component to use a hardcoded room.

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -6,45 +6,9 @@ import Chat from "./Chat";
 const socket = io.connect("https://chat-app-server-00jj.onrender.com");
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
-
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-
-      socket.emit("join_room", room);
-      setShowChat(true);
-
-
-    }
-  };
-
+  const room = "SSG";
   return (
-    <div className="App">
-      {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          <input
-            type="text"
-            placeholder="Enter Your Name "
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
-        </div>
-      ) : (
         <Chat socket={socket} username={username} room={room} />
-      )}
-    </div>
   );
 }
 
